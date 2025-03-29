598de29e339b6e68b64ed3b333ba2be9c1b894c8
Imran9001 <122745336+Imran9001@users.noreply.github.com>
2025-03-29 17:01:01 +0000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+nickname = input("Choose a nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('5.tcp.eu.ngrok.io', 15926))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                pass
+            else:
+                print(message)
+
+        except:
+            print("An error occurred")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target = receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target = write)
+write_thread.start()
